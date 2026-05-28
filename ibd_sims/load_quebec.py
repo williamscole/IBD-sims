223203251339d3a8489e60fe6f655126011a7831
cole_williams@brown.edu <cwilli50@node2309.oscar.ccv.brown.edu>
2026-05-27 22:30:10 -0400

    big in quebec tree generation

--- ibd_sims/load_quebec.py
+++ ibd_sims/load_quebec.py
@@ -3,28 +3,85 @@ import numpy as np
 import pickle as pkl
 import sys
 import msprime
+import itertools as it
 from simulations import GenomeSetup
 
 CHROM22 = "/gpfs/data/sramacha/datasets/quebec_sims/simulated_chrom_22.ts.tsz"
 
 PATH = "quebec_trees"
 
-def simplify_quebec(chrom, args, seed=42):
+def get_node_table(ts):
+
+    # get the nodes corresponding to the 0th generation and create a pandas dataframe
+    node_table = ts.dump_tables().nodes
+    nodes = pd.DataFrame({"time": node_table.time, "individual": node_table.individual})
+
+    # get nodes from cur gen
+    cur_gen_nodes = nodes[nodes.time==0]
+
+    # get the individual name
+    cur_gen_nodes["id1"] = cur_gen_nodes["individual"].apply(lambda x: ts.individual(x).metadata["individual_name"])
+
+    return cur_gen_nodes
+
+def load_chr21():
+
     import tszip
 
+    print("Loading chromosome 21")
+    ts = tszip.decompress(CHROM22.replace("_22", "_21"))
+    print("\tLoaded.")
+
+    cur_gen_nodes = get_node_table(ts)
+
+    # get the individuals
+    keep_nodes = cur_gen_nodes.drop_duplicates("id1").sample(50000)["id1"]
+
+    # write out
+    keep_nodes.to_csv(f"{PATH}/random_ids.txt", header=False, index=False)
+
+def simplify_quebec(chrom, seed=42):
+    import tszip
+
+    keep_nodes = pd.read_csv(f"{PATH}/random_ids.txt", header=None)
+    keep_ids = keep_nodes[0].apply(str).values 
+
+    print(f"Loading chromosome {chrom}")
     ts = tszip.decompress(CHROM22.replace("_22.", f"_{chrom}."))
+    print("\tLoaded.")
 
-    n_samples = np.where(ts.nodes_time==0)[0].shape[0] // 2
+    node_df = get_node_table(ts)
 
-    np.random.seed(42)
+    id_to_node = node_df.reset_index().groupby('id1')['index'].min().to_dict()
+    chr_nodes_to_keep = list(it.chain(*[[id_to_node[pid], id_to_node[pid]+1] for pid in keep_ids]))
+
+    ts = ts.simplify(samples=chr_nodes_to_keep)
+
+    tszip.compress(ts, f"{PATH}/chr{chrom}_random_50000.trees.tsz")
+
+def load_random_50000(chrom, args):
+
+    import tszip
+
+    iteration_seed = args.get("iteration_seed", 1000)
+
+    np.random.seed(iteration_seed)
+
+    ts = tszip.decompress(f"{PATH}/chr{chrom}_random_50000.trees.tsz")
+
+    node_df = get_node_table(ts)
+
+    n_samples = node_df.shape[0] // 2
 
     random_nodes = np.random.choice(np.arange(n_samples), args.get("samples", 1000), replace=False) * 2
-
     random_nodes = np.sort(np.concatenate((random_nodes, random_nodes+1)))
 
-    ts = ts.simplify(samples=random_nodes)
-    
-    ts.dump(f"{PATH}/chr{chrom}_random_10000.trees")
+    if len(random_nodes) < (n_samples * 2):
+        ts = ts.simplify(samples=random_nodes)
+
+    _, rate = GenomeSetup.create(args, chrom)
+
+    return ts, rate
 
 def load_random_10000(chrom, args):
 
@@ -49,6 +106,10 @@ def load_random_10000(chrom, args):
 
 if __name__ == "__main__":
 
-    for i in range(1, 23):
-        simplify_quebec(i, {"samples": 10000})
+    if sys.argv[1] == "init":
+        load_chr21()
+
+    else:
+        simplify_quebec(int(sys.argv[1]))
+
 
